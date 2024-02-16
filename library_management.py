047e59ffd532b77f2683556760ee455bc7edee43
ceydaiistanbullu <ceydaistanbullu13@gmail.com>
2024-02-16 21:44:51 +0300

    first commit

---
+++ library_management.py
@@ -0,0 +1,20 @@
+class Library():
+    def __init__(self):
+        self.file = open("books.txt","a+") #dosyayı acıyorum yoksa olusturuyorum.
+
+
+    def __del__(self):
+        self.file.close()  #kutuphane nesnesi silinince dosyayı kapatıyorum.
+        
+
+
+def list_books(self):
+        self.file.seek(0)  # dosyanın basına don
+        books = self.file.readlines()
+        if not books:
+            print("Kütüphane de kitap bulunmuyor.")
+        else:
+            print("Kütüphanede ki kitaplar:")
+            for book in books:
+                book_info = book.strip().split(",")
+                print(f"Title: {book_info[0]}, Author: {book_info[1]}")
