a6babaab5726a8e84c8e11b40d3da0db2f8cd084
ceydaiistanbullu <ceydaistanbullu13@gmail.com>
2024-02-18 03:10:10 +0300

    hersey calısıyor

--- library.py
+++ library.py
@@ -10,14 +10,14 @@ class Library():
 
     def list_books(self):
         self.filename.seek(0)  # dosyanın basına don
-        books = self.file.readlines()
-        if not books:
+        self.books = self.filename.readlines()
+        if not self.books:
              print("Kütüphane de kitap bulunmuyor.")
         else:
             print("Kütüphanede ki kitaplar:")
-            for book in books:
+            for book in self.books:
                 book_info = book.strip().split(",")
-                print(f"Title: {book_info[0]}, Author: {book_info[0]}") 
+                print(f"{book_info[0]}, {book_info[1]}, {book_info[2]}, {book_info[3]}") 
 
 
     def add_books(self):
@@ -31,52 +31,54 @@ class Library():
 
     def remove_books(self):
         title = input("Enter the title of the book you want to remove: ")
-        self.file.seek(0)  # sıfırlıyor
-        books = self.file.readlines()
+        self.filename.seek(0)  # sıfırlıyor
+        self.books = self.filename.readlines()
         found = False
-        for i, book in enumerate(books):
-            if title in book:
-                del books[i]
+        for i, book in enumerate(self.books):
+            if str(title) in str(book):
+                del self.books[i]
                 found = True
                 break
         if found:
-            self.file.seek(0)
-            self.file.truncate()  #dosyanın icini siliyor
-            self.file.writelines(books)  #güncellenmis kitap listesini dosyaya yazar
+            self.filename.seek(0)
+            self.filename.truncate()  #dosyanın icini siliyor
+            self.filename.writelines(self.books)  #güncellenmis kitap listesini dosyaya yazar
             print(f"The book '{title}' has been removed from the library.")
+            # print(self.books)
         else:
             print(f"The book '{title}' is not found in the library.")
 
 
-#lib = Library()
 
-#lib.add_books()
-#lib.remove_books()
+def main():
+    lib = Library()
 
-    def menu_göster(self):
-        print("\nKütüphane Yönetim Sistemine Hoşgeldiniz!")
-        print("1. Kitapları Listele")
-        print("2. Kitap Ekle")
-        print("3. Kitap Sil")
-        print("Q Çıkış")
+    print("\nKütüphane Yönetim Sistemine Hoşgeldiniz!")
+    print("1. Kitapları Listele")
+    print("2. Kitap Ekle")
+    print("3. Kitap Sil")
+    print("Q Çıkış")
+    
+    
+    #while True:
 
-#Library = Library("books.txt")
-#Library.add_books()
-#Library.remove_books()
-#Library.ana_menu_göster()
-#Library.list_books()
 
-while True:
-    menu_göster()
     choise = input("Yapmak istediğiniz işlemi seçiniz: ")
+    print("----------------")
 
     if choise == "1":
-        list_books()
+        lib.list_books()
     elif choise == "2":
-        add_books()
+        lib.add_books()
     elif choise == "3":
-        remove_books()
+        lib.remove_books()
     elif choise == "Q":
-        break
+        exit()
     else:
         print("Geçersiz seçim. Lütfen geçerli bir seçenek giriniz.")
+
+
+
+if __name__ == "__main__":
+    main()
+   
